b1656d39735563c93ce97f66fde8ff03e043273e
Juhasz Zsolt <juhasz.zsolt98@gmail.com>
2022-04-02 15:59:46 +0200

    Validalasok beepitve

--- src/app/member/member.component.ts
+++ src/app/member/member.component.ts
@@ -1,5 +1,15 @@
+/*
+* File: member.component.ts
+* Author: Juhász Zsolt
+* Copyright: 2022, Juhász Zsolt
+* Group: Szoft II/N
+* Date: 2022-04-02
+* Github: https://github.com/ZsoltJuhasz/member
+* Licenc: GNU GPL
+*/
+
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-member',
@@ -8,11 +18,11 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class MemberComponent implements OnInit {
 
-  memberForm = new FormGroup( {
-    name: new FormControl(''),
-    city: new FormControl(''),
-    address: new FormControl(''),
-    email: new FormControl('')
+  memberForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    city: new FormControl('', Validators.required),
+    address: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email])
   })
 
   constructor() { }
@@ -22,12 +32,12 @@ export class MemberComponent implements OnInit {
 
   saveMember() {
     alert(
-      'Név: ' + this.memberForm.value.name + '\n' + 
+      'Név: ' + this.memberForm.value.name + '\n' +
       'Település:' + this.memberForm.value.city + '\n' +
       'Cím:' + this.memberForm.value.address + '\n' +
       'E-mail:' + this.memberForm.value.email
 
-      );
+    );
   }
 
 }
